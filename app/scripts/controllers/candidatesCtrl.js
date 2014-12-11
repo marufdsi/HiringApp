faf5aec9212388c575be6f6d90bb0c8ec9e77e9d
Yeasin <yeasinhossain-dsi@gmail.com>
2014-12-11 12:28:15 +0600

    update

--- app/scripts/controllers/candidatesCtrl.js
+++ app/scripts/controllers/candidatesCtrl.js
@@ -1,9 +1,14 @@
 app.controller('candidatesCtrl', ['$scope', 'candidatesResolve', 'appConfigFactory', function( $scope, candidatesResolve, appConfigFactory ){    
     
     $scope.candidates = candidatesResolve.data.data;
+    $scope.max = 10;
     $scope.candidate_image_dir = appConfigFactory.CANDIDATE_IMG;
     $scope.candidate_resume_dir = appConfigFactory.CANDIDATE_RESUME;
     $scope.profile_thumb = appConfigFactory.PROFILE_THUMB;
     $scope.profile_icon = appConfigFactory.PROFILE_ICON;
     
+    
+    $scope.recommendModal = function( candidate ){
+        console.log( candidate );
+    }
 }]);
